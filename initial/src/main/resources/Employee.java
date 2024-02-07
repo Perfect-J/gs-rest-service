e4b4cb19c45ad8018ae579c31193d974416a760e
Johnson Fadogba <johnsonayo532@gmail.com>
2024-02-07 13:26:28 +0100

    Create Employee.java

---
+++ initial/src/main/resources/Employee.java
@@ -0,0 +1,45 @@
+{
+
+    “Employees: [
+
+         {
+
+             “employee_id”: “string”,
+
+             “first_name”: “string”,
+
+             “last_name”: “string”,
+
+             “email”: “string”,
+
+             “title”: “string”
+
+         },
+
+             “employee_id”: “string”,
+
+             “first_name”: “string”,
+
+             “last_name”: “string”,
+
+             “email”: “string”,
+
+             “title”: “string”
+
+         {
+
+             “employee_id”: “string”,
+
+             “first_name”: “string”,
+
+             “last_name”: “string”,
+
+             “email”: “string”,
+
+             “title”: “string”
+
+         }
+
+    ]
+
+}
